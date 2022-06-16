41d74b304586745b7c24310d655f07e777c3e7a7
yanis khellaf <yanis.khellaf@outlook.fr>
2022-06-16 10:22:03 +0200

    review routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, except: [:edit, :update] do
       resources :bookmarks, only: [:new, :create]
+      resources :reviews, only: :create
   end
   resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
